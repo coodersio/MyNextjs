19e8af25e3a3ced5798e3a5a94e390476e2a7f6f
wenqiang.zou <wenqiang.zou@hexagon.com>
2024-08-20 14:57:21 +0800

    Dinit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,6 @@
+import '@/app/ui/global.css'
+import { open_sans} from '@/app/ui/fonts'
+
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +8,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${open_sans.className} antialiased`}>{children}</body>
     </html>
   );
 }
